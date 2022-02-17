3d0d70711f376e1f0edfc8f1fdbd059395e29a35
Tariqul Islam <usukuk@gmail.com>
2022-02-17 16:03:45 +0600

    refactoring step 1

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ const foodInput = document.getElementById("food-input");
 const rentInput = document.getElementById("rent-input");
 const clothesInput = document.getElementById("clothes-input");
 const savingInput = document.getElementById("saving-input");
-
 // Get All buttons
 const btnIncExp = document.getElementById("btn-inc-exp");
 const btnSaving = document.getElementById("btn-saving");
-
 // Get All Output Displays
 const totalExp = document.getElementById("total-exp");
 const balance = document.getElementById("balance");
@@ -19,23 +17,20 @@ const errorDisplays = document.getElementsByClassName("error-display");
 // Global Vars
 let balanceValue = 0;
 let incomeInputValue = 0;
-let errors = {};
+const errors = {};
 
 //////////// Calculate Balance with Income and Expenses //////////////
 btnIncExp.addEventListener("click", (e) => {
-  // preventing default behavior
-  e.preventDefault();
+  initialActions(e);
 
-  // clear error displays
-  clearErrorDisplays();
-
+  // Validate inputs
   validateInput(incomeInput, "Income should be a number and greater than 0", 1);
   validateInput(foodInput, "food cost should be a number");
   validateInput(rentInput, "food cost should be a number");
   validateInput(clothesInput, "clothes cost should be a number");
 
   // return if there is an error
-  if (Object.keys(errors).length > 0) return displayThenClearErrors();
+  if (Object.keys(errors).length > 0) return displayErrorsThenClearErrors();
 
   // local vars
   incomeInputValue = parseInt(incomeInput.value);
@@ -51,7 +46,7 @@ btnIncExp.addEventListener("click", (e) => {
   // return if expenses is greater than income
   if (totalExpValue > incomeInputValue) {
     errors[incomeInput.id] = "Income should be greater than total Expenses";
-    return displayThenClearErrors();
+    return displayErrorsThenClearErrors();
   }
 
   // Displaying total expenses on UI
@@ -62,25 +57,23 @@ btnIncExp.addEventListener("click", (e) => {
 
 ////////////// Calculate Saving with balance and saving percentage /////////////
 btnSaving.addEventListener("click", (e) => {
-  // preventing default behavior
-  e.preventDefault();
-  // clear error displays
-  clearErrorDisplays();
+  initialActions(e);
 
   // validate input
   validateInput(
     savingInput,
-    "Saving Percent should be a number, and less than 100",
+    "Saving Percent should be a number, and less than or equal to 100",
     0,
-    99
+    100
   );
 
   // validate again in balance is 0 or less
-  if (balanceValue <= 0)
-    validateInput(savingInput, "Please calculate balance first");
+  if (balanceValue <= 0) {
+    validateInput(savingInput, "You cannot save when your balance is zero");
+  }
 
   // return if there is an error
-  if (Object.keys(errors).length > 0) return displayThenClearErrors();
+  if (Object.keys(errors).length > 0) return displayErrorsThenClearErrors();
 
   // local vars
   const savingInputValue = parseInt(savingInput.value);
@@ -88,16 +81,17 @@ btnSaving.addEventListener("click", (e) => {
   // calculating saving
   const savingPercent = savingInputValue / 100;
   const savingAmountValue = incomeInputValue * savingPercent;
-  const availableSavingPercent = (balanceValue / incomeInputValue) * 100;
 
   // return if saving amount more than the balance you have
   if (savingAmountValue > balanceValue) {
-    errors[savingInput.id] = `You cannot save more than ${Math.floor(
-      availableSavingPercent
-    )} percent of your income`;
+    const availableSavingPercent = Math.floor(
+      (balanceValue / incomeInputValue) * 100
+    );
 
-    // return if there is an error
-    if (Object.keys(errors).length > 0) return displayThenClearErrors();
+    const errorText = `You cannot save more than ${availableSavingPercent} percent of your income`;
+    errors[savingInput.id] = errorText;
+
+    return displayErrorsThenClearErrors();
   }
 
   // Calculate remaining Balance
@@ -105,12 +99,17 @@ btnSaving.addEventListener("click", (e) => {
 
   // Display saving amount on UI
   savingAmount.innerText = savingAmountValue.toFixed(2);
-
   // Display remaining balance amount on UI
   remainingBalance.innerText = remainingBalanceValue.toFixed(2);
 });
 
-// validate input
+function initialActions(e) {
+  // preventing default behavior
+  e.preventDefault();
+  // clear error displays
+  clearErrorDisplays();
+}
+
 function validateInput(domInput, errorText, min = 0, max = Infinity) {
   if (
     isNaN(parseInt(domInput.value)) ||
@@ -121,26 +120,19 @@ function validateInput(domInput, errorText, min = 0, max = Infinity) {
   }
 }
 
-// display then clear errors
-function displayThenClearErrors() {
-  // Display  errors
+function displayErrorsThenClearErrors() {
   displayErrors();
-
-  // Clear Errors
   clearErrors();
 }
 
-// clear all errors
 function clearErrors() {
   for (const key in errors) {
     delete errors[key];
   }
 }
 
-// display errors in dom
 function displayErrors() {
   for (const err in errors) {
-    // console.log(err);
     const target = document.getElementById(err + "-error");
     // show error
     target.classList.remove("hidden");
@@ -149,13 +141,8 @@ function displayErrors() {
   }
 }
 
-// clear all dom error display
 function clearErrorDisplays() {
   for (const ed of errorDisplays) {
     ed.classList.add("hidden");
   }
-}
-
-function insertAfter(referenceNode, newNode) {
-  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
